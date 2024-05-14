9d49f35e3c5a9211f56a0a68c33d52673afb37aa
Rupak Chatterjee <rupakchttrj09@gmail.com>
2024-05-14 23:55:18 +0530

    for genre titles having space made a fix

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import "./button.css"
 
 const Button = ({title, onButtonClick, isSelected}) => {
+  let constructedTitle
+
+  if(title.includes(" ")){
+    let tmp = title.split(" ")
+    constructedTitle = <p>{tmp[0]}&nbsp;{tmp[1]}</p>
+  }else{
+    constructedTitle = title
+  }
+
   return (
-    <button onClick={onButtonClick} className={isSelected ? 'button_common': 'button_outline'}>{title}</button>
+    <button onClick={onButtonClick} className={isSelected ? 'button_common': 'button_outline'}>{constructedTitle}</button>
   )
 }
 
